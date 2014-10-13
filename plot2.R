87b1149c7bbbbdbfe4ca8b8c760143bfd6a73415
alok <alokd3@yahoo.com>
2014-10-12 21:22:01 -0400

    first commit

---
+++ plot2.R
@@ -0,0 +1,12 @@
+#loading source data and then base graph
+source("power_data.R")
+plot2 <- function(data=NULL) {
+if(is.null(data))
+data <- power_data()
+png("plot2.png", width=400, height=400)
+plot(data$Time, data$Global_active_power,
+type="l",
+xlab="",
+ylab="Global Active Power (kilowatts)")
+dev.off()
+}
